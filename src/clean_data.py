7e8919ba81cb9b604af323877f583ba10c1f62ef
mahapsub <smahapat@u.northwestern.edu>
2019-04-20 19:26:17 -0500

    finished assignment 1

--- src/clean_data.py
+++ src/clean_data.py
@@ -3,50 +3,108 @@ import matplotlib.pyplot as plt
 import glob
 import cv2
 import math
+import copy
 
 def main():
-    image_lst = parse_data()
-    # limg = image_lst[0]
-    # rimg = image_lst[1]
-    # # display_image([limg, rimg])
-    # print(limg.shape)
-    # print(rimg.shape)
-    # img = rgb2gray(limg)
-    # flat_img = img.flatten()
-    # print(len(flat_img))
-    # dim = int(math.sqrt(len(flat_img)))
+    images = parse_data()
+    if len(images)==0:
+        print('No images found')
+        return False
+    thresh = np.zeros((2032,2032,3))
+
+    if len(images)> 80:
+        image_lst = images[:80]
+    else:
+        image_lst = images
 
 
-    # plt.imshow(flat_img.reshape((dim,-1)), cmap="gray")
-    # plt.show()
-    threshold_img = np.zeros((2032,2032))
-    print(threshold_img.shape)
-    print(type(threshold_img))
     for i in range(len(image_lst)):
-        img = rgb2gray(image_lst[i])
+        img = image_lst[i]
         # print(type(img))
         # print(img.shape)
-        threshold_img +=img
-    # threshold_img = int(threshold_img/len(image_lst))
-    threshold_img = cv2.equalizeHist(threshold_img)
-    kernel = np.ones((50,50),np.float32)/(50*50)
-    thresh = cv2.filter2D(threshold_img,-1,kernel)
-    thresh = cv2.medianBlur(np.float32(thresh),5)
-    # thresh = cv2.threshold(threshold_img, 110, 255, cv2.THRESH_BINARY)[1]
-    # thresh = cv2.adaptiveThreshold(threshold_img,100,cv2.ADAPTIVE_THRESH_MEAN_C,\
-            # cv2.THRESH_BINARY,11,2)
-    thresh = cv2.erode(thresh, None, iterations=2)
-    thresh = cv2.dilate(thresh, None, iterations=4)
+        # img = cv2.cvtColor(image_lst[i], cv2.COLOR_BGR2HSV)
+        thresh += img
+    thresh = thresh/len(image_lst)
+    
+    
+
+    thresh = np.array(np.round(thresh),dtype=np.uint8)
+    #plot1
+    mean = thresh
+
+    # cv2.imwrite('01.png',thresh)
+    # print(thresh.shape)
+    # print(thresh)
+    median_blur_size = 3
+    thresh = cv2.medianBlur(thresh,median_blur_size)
+    thresh = cv2.cvtColor(thresh, cv2.COLOR_BGR2GRAY)
+
+    #plot2
+    bw_medianblur = thresh
+
+    _,thresh1 = cv2.threshold(thresh, 100, 255, cv2.THRESH_BINARY)
+    thresh2 = cv2.adaptiveThreshold(thresh,255,cv2.ADAPTIVE_THRESH_MEAN_C,\
+            cv2.THRESH_BINARY,107,13)
+
+    # blur = cv2.GaussianBlur(thresh,(5,5),0)
+    # ret3,thresh4 = cv2.threshold(blur,105,255,cv2.THRESH_BINARY+cv2.THRESH_OTSU)
+    
+
+    mask = cv2.bitwise_not(thresh2)
+
+    #plot3
+    mask_found = mask
+    # contours, hierarchy = cv2.findContours(mask,cv2.RETR_TREE,cv2.CHAIN_APPROX_SIMPLE)
+    # cnt = contours[0]
+    M = cv2.moments(mask)
+
+    #Uses the entire folder to find a random image
+    trial_img = images[np.random.randint(0,len(images))]
+    #plot4 
+    random_orig = copy.deepcopy(trial_img)
+
+    #moo checks the size of the blur.
+    if check_size(M):
+    # if 'm00' in M:
+        cX = int(M["m10"] / M["m00"])
+        cY = int(M["m01"] / M["m00"])
+        # put text and highlight the center
 
 
-    plt.imshow(thresh, cmap="gray")
+        cv2.circle(mask, (cX, cY), 75, (100, 100, 100), 5)
+        cv2.putText(mask, "Smudge", (cX - 120, cY ),cv2.FONT_HERSHEY_SIMPLEX, 2, (145, 76, 0), 4)
+        #Applying mask
+        cv2.circle(trial_img, (cX, cY), 75, (100, 100, 100), 5)
+        #Applying Text
+        cv2.putText(trial_img, "Smudge", (cX - 120, cY ),cv2.FONT_HERSHEY_SIMPLEX, 2, (145, 76, 0), 4)
+
+    fig, axs = plt.subplots(2, 3, figsize=(50, 30), sharey=True)
+    axs[0][0].imshow(mean)
+    axs[0][0].set_title('The average of {} imgs'.format(len(image_lst)))
+    axs[0][1].imshow(bw_medianblur)
+    axs[0][1].set_title('post analysis at {}'.format(median_blur_size))
+    axs[0][2].imshow(thresh1)
+    axs[0][2].set_title('Simple binary threshold')
+    axs[1][0].imshow(mask_found)
+    axs[1][0].set_title('Checking to see if mask is found')
+    axs[1][1].imshow(random_orig)
+    axs[1][1].set_title('Selecting random image from corpora')
+    axs[1][2].imshow(trial_img)
+    axs[1][2].set_title('Application of mask to random image')
     plt.show()
-    
-        
-    
+
+def check_size(M):
+    size = M['m00']
+    # print('SIZE', size)
+    if size > 50000 and  size < 250000:
+        return True
+    else:
+        return False
+
 
 def rgb2gray(rimg):
-    return cv2.cvtColor(rimg, cv2.COLOR_BGR2GRAY)
+    return rimg
+    # return cv2.cvtColor(rimg, cv2.COLOR_BGR2GRAY)
     # r = rimg[:,:,0]
     # g = rimg[:,:,0]
     # b = rimg[:,:,0]
@@ -59,18 +117,23 @@ def parse_data():
     '''
         Reads in data and parses dataset
     '''
+    dir_path = '/Users/mahapsub/workspace/msai/geospatial/smudge_detection_images/data/sample_drive/cam_3/'
 
-    dir_path = '/Users/mahapsub/workspace/msai/geospatial/data/sample_drive/cam_3/'
-    print('reading images')
+    print('reading images from \'{}\''.format(dir_path))
     image_names = [file for file in glob.glob(dir_path+"*.jpg")]
-    sample_size = 0.05
+    sample_size = 0.020
     sz = int(len(image_names)*sample_size)
-    print('size of images = ',sz)
+    
     sample_images = image_names[0:sz]
     # print('image names:{}'.format([name for name in sample_images]))
 
-    sample_lst = [cv2.imread(file) for file in sample_images]
-    print('done reading')
+    sample_lst = []
+    for i in range(len(sample_images)):
+        sample_lst.append(cv2.imread(sample_images[i]))
+        if i % (int(len(sample_images)*.10)) == 0:
+            print('loaded {0} images out of {1}'.format(i, len(sample_images)))
+
+    # sample_lst = [cv2.imread(file) for file in sample_images]
 
     return sample_lst
 
